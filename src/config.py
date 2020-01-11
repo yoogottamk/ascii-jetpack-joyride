b740fd10f17ab4bf47b5f5f5edb1a5801d9db193
YoogottamK <yoogottamk@outlook.com>
2020-01-11 16:47:33 +0530

    tweaked config

--- src/config.py
+++ src/config.py
@@ -7,9 +7,17 @@ import colorama as col
 
 SCREEN_HEIGHT, SCREEN_WIDTH = [int(x) for x in os.popen("stty size", "r").read().split()]
 
+#WIDTH = SCREEN_WIDTH - 10
+HEIGHT = SCREEN_HEIGHT - 5
+
+WIDTH = int(SCREEN_WIDTH / 2)
+#HEIGHT = int(SCREEN_HEIGHT / 1.5)
+
 # default colors
 bg_col = col.Back.BLUE
 fg_col = col.Fore.BLACK
+
+bg_ground = col.Back.GREEN
 
 # delay bw frame updates
 delay = 0.1
