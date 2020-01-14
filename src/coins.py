429020339ee1a1901f759e9b02f2e277ca3e5567
YoogottamK <yoogottamk@outlook.com>
2020-01-14 11:58:31 +0530

    Added coins docs

--- src/coins.py
+++ src/coins.py
@@ -9,7 +9,18 @@ from objects import GameObject
 import util
 
 class Coins(GameObject):
+    """
+    This class is for all the coins
+    """
+
     def __init__(self, position, shape):
+        """
+        Initializes the coins object
+
+        Args:
+            position [px, py]     : Position where the coins will be placed
+            shape [height, width] : Shape of the bundle of coins
+        """
         rep = np.full(shape, "$")
         color = util.tup_to_array(rep.shape, (col.Back.YELLOW, col.Fore.RED))
 
@@ -17,6 +28,12 @@ class Coins(GameObject):
                 np.array([0., 0.]), 0, color)
 
     def update(self):
+        """
+        This function updates the object after each frame
+
+        Returns:
+            bool : Should the object be rendered in the next frame
+        """
         self.position += self.velocity
 
         return self.position[0] + self.width >= 0
